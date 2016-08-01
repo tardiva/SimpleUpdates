e3b75c39d6c2ef869a0ec7198edfa25b5424bd5d
Irina Romanova <irina.romanova@datanaut.com>
2016-08-01 19:54:08 +0300

    projects create and delete functionality in progress

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,41 +5,61 @@ import { Updates } from '../api/projects.js';
 import './body.html';
 
 
-    class Dropdown {
+class Dropdown {
 
-        constructor (el) {
+    constructor (el) {
 
-            this.dd = el;
-            this.btn = el.getElementsByClassName('button-dd').item(0);
-            this.menu = el.getElementsByClassName('menu-dd').item(0);
-            //this.opts = ;
+        this.dd = el;
+        this.btn = el.getElementsByClassName('button-dd').item(0);
+        this.menu = el.getElementsByClassName('menu-dd').item(0);
+        //this.opts = ;
         }
 };
+
+function getID () {
+    
+    let lastProject = Projects.find({}, {sort: {id: -1}, limit: 1}).fetch()[0];
+            
+    if (lastProject == undefined) {return 1}
+        else {return (parseInt(lastProject.id) + 1)};
+};
+
 
 Template.body.helpers({
 
     projects () {
-           return Projects.find({});},
+        return Projects.find({});
+    },
     
     updates () {
-           return Updates.find({});},
+        return Updates.find({});
+    },
+    
+    hasUpdates (project) {
+         if (Updates.find({project_id: {$eq: project.id}}).count() < 1) {return false}
+               else {return true};
+    },
     
     lastUpdate (project) {
-          let id = project.id;
-          let upd = Updates.find({project_id: {$eq: id}});
-          console.log(upd.count() + " " + project.id);
-          if (upd.count() == 0) {
-              return false;}
-          else {
-              return Updates.find({project_id: { $eq: id}}, {sort: {createdAt: -1}, limit: 1});
-          }
+        return Updates.find({project_id: { $eq: project.id}}, {sort: {createdAt: -1}, limit: 1}).fetch()[0];
+          
       }, 
+    
+    noUpdates () {
+        update = {
+          createdAt: "",
+          priority: "",
+          text: "no updates for this project"
+        };
+        return update;
+    }
 });
 
 Template.projectItem.helpers({      
 
     formatDate (date) {
-         return moment(date).format('LL');},
+        if (date instanceof Date){
+        return moment(date).format('LL')};},
 
     colorCycle (update) {
         let color = "";
@@ -55,80 +75,87 @@ Template.projectItem.helpers({
 
 });
 
-    Template.body.events({
+Template.body.events({
 
-       /* 'mouseover .wrapper-dd, mouseout .wrapper-dd' (event) {
+    'mouseleave .wrapper-dd' (event) {
         
         let dropdown = new Dropdown(event.target);
-        dropdown.menu.classList.toggle('active');
-        console.log('active!')
-
-    },*/
+        dropdown.menu.classList.remove('active');
+    },
         
-        'click .button-dd' (event) {     
+    'click .button-dd' (event) {     
 
-        /*let dd = event.target,
-            menu = dd.parentElement.getElementsByClassName('menu_dd').item(0);
-
+        /*let menu = event.target.parentElement.getElementsByClassName('menu_dd').item(0);
         menu.classList.toggle('active');*/
 
         let dropdown = new Dropdown(event.target.parentElement);
         dropdown.menu.classList.toggle('active');
-        console.log('active!')
-        },
-
-       /* 'mouseout .menu-dd' (event) {
-            
-            let menu = event.currentTarget;
-
-            menu.classList.toggle('active');
-
-            console.log('mouseout menu - ' + menu);
-
-        },*/
+    },   
         
-        'mouseover .menu-opt, mouseout .menu-opt' (event) {
-                    
-            let opt = event.target;
-
-            opt.classList.toggle('highlight');
-            //event.stopPropagation();
-            console.log('menu_opt highlight - it works!');
-        },
-
-        'click .menu-opt' (event) {
-            
-            let opt = event.target,
-                button = opt.parentElement.parentElement.getElementsByClassName('button-dd').item(0);
-            button.setAttribute("value", opt.getAttribute("value"));
-            button.textContent = opt.textContent;  
-            
-            console.log(opt);
-       },
-
-
-        'click button.new-update-btn'(event) {
-
-            event.preventDefault();
-
-            let textField = document.getElementById("update-text"),
-                projectsList = document.getElementById("update-project"),
-                priorityList = document.getElementById("update-priority");
+    'mouseover .menu-opt, mouseout .menu-opt' (event) {
         
-            Updates.insert({
-                createdAt: new Date(),
-                text: textField.value,
-                project_id: projectsList.getAttribute("value"),
-                priority: priorityList.getAttribute("value") });
+        event.target.classList.toggle('highlight');
+    },
 
-            textField.value = '';
-            projectsList.setAttribute("value", "0");
-            projectsList.textContent = "Projects";
-            priorityList.setAttribute("value", "0");
-            priorityList.textContent = "Priority";
+    'click .menu-opt' (event) {
+            
+        let opt = event.target,
+            button = opt.parentElement.parentElement.getElementsByClassName('button-dd').item(0);
+        button.setAttribute("value", opt.getAttribute("value"));
+        button.textContent = opt.textContent;
+        opt.closest(".menu-dd").classList.remove("active");
+    },
+
+
+    'submit form.add-update'(event) {
+
+        event.preventDefault();
+
+        let textField = document.getElementById("update-text"),
+            projectsList = document.getElementById("update-project"),
+            priorityList = document.getElementById("update-priority");
         
-            console.log('update!');
-        },
+        Updates.insert({
+            createdAt: new Date(),
+            text: textField.value,
+            project_id: parseInt(projectsList.getAttribute("value")),
+            priority: priorityList.getAttribute("value") });
 
-    });
+        textField.value = '';
+        projectsList.setAttribute("value", "0");
+        projectsList.textContent = "Projects";
+        priorityList.setAttribute("value", "0");
+        priorityList.textContent = "Priority";
+    },
+        
+    'submit form.add-project' (event) {
+        
+        event.preventDefault();
+                        
+        Projects.insert({
+            id: getID(),
+            name: document.getElementById("add-project-name").value
+        });
+    },
+        
+    'click .delete-btn' (event) {
+            
+        let id = this.project.id;
+        Meteor.call('removeUpdates', id);
+        Meteor.call('removeProject', id);
+    },
 
+});
+
+/*Meteor.methods({
+    
+    'removeProject': function (id) {
+        Projects.remove({id: id});
+    },
+    
+    'removeUpdates': function (projectId) {
+       Updates.remove({project_id: projectId});
+    },
+        
+});
+*/
--- server/main.js
+++ server/main.js
@@ -1,6 +1,22 @@
-import { Meteor } from 'meteor/meteor';
-import '../imports/api/projects.js';
+import {Meteor} from 'meteor/meteor';
+//import  '../imports/api/projects.js';
+import {Projects, Updates} from  '../imports/api/projects.js';
+
 
 Meteor.startup(() => {
-  // code to run on server at startup
+ 
+  
+});
+
+Meteor.methods({
+    
+    'removeProject': function (id) {
+         Projects.remove({id: id});
+        
+    },
+    
+    'removeUpdates': function (projectId) {
+         Updates.remove({project_id: projectId});
+    },
+        
 });
